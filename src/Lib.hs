443526d249b23c8df1047ae54aeb76de01b60d08
Chris Penner <christopher.penner@gmail.com>
2021-11-07 22:11:51 -0600

    Good start on things

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,6 +7,8 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Lib where
 
@@ -33,6 +35,7 @@ import Graphics.Vty
 import qualified Graphics.Vty as Vty
 import Pretty
 import qualified Zipper as Z
+import Control.Monad.State
 
 data FocusState = FS
   { keySelection :: Maybe Text,
@@ -44,19 +47,19 @@ run = do
   void $ edit $ fromJust $ Aeson.decode ("[\"hi\", {\"testing\":[1, 2, false, null]}, [4, 5, 6]]")
 
 edit :: Value -> IO Value
-edit value = do
-  config <- standardIOConfig
-  vty <- mkVty config
+edit value = flip evalStateT (FS Nothing Nothing) $ do
+  config <- liftIO $ standardIOConfig
+  vty <- liftIO $ mkVty config
   let start = fromJust $ (Z.down (Index 0)) . Z.zipper . renderedZipper . toCofree $ value
   let loop foc = do
-        update vty . Vty.picForImage $ renderValue' foc
-        e <- nextEvent vty
-        let foc' = handleEvent foc e
+        liftIO $ update vty . Vty.picForImage $ renderValue' foc
+        e <- liftIO $ nextEvent vty
+        foc' <- handleEvent foc e
         if (maybeQuit e)
           then pure foc'
           else (loop foc')
   v <- loop start
-  Vty.shutdown vty
+  liftIO $ Vty.shutdown vty
   pure (Z.flatten v)
 
 maybeQuit :: Vty.Event -> Bool
@@ -64,40 +67,15 @@ maybeQuit = \case
   EvKey (KChar 'c') [Vty.MCtrl] -> True
   _ -> False
 
-handleEvent :: Z.Zipper ValueF a -> Vty.Event -> Z.Zipper ValueF a
+handleEvent :: MonadState FocusState m => Z.Zipper ValueF a ->  Vty.Event -> m (Z.Zipper ValueF a)
 handleEvent z = \case
   EvKey key mods -> case key of
-    KChar 'h' -> Z.tug Z.up z
+    KChar 'h' -> pure $ Z.tug Z.up z
     KChar 'l' -> into z
-    KChar 'j' -> nextSibling z
-    KChar 'k' -> prevSibling z
-    _ -> z
-  -- KChar c -> _
-  -- KEsc -> _
-  -- KBS -> _
-  -- KEnter -> _
-  -- KLeft -> _
-  -- KRight -> _
-  -- KUp -> _
-  -- KDown -> _
-  -- KUpLeft -> _
-  -- KUpRight -> _
-  -- KDownLeft -> _
-  -- KDownRight -> _
-  -- KCenter -> _
-  -- KFun n -> _
-  -- KBackTab -> _
-  -- KPrtScr -> _
-  -- KPause -> _
-  -- KIns -> _
-  -- KHome -> _
-  -- KPageUp -> _
-  -- KDel -> _
-  -- KEnd -> _
-  -- KPageDown -> _
-  -- KBegin -> _
-  -- KMenu -> _
-  _ -> z
+    KChar 'j' -> pure $ nextSibling z
+    KChar 'k' -> pure $ prevSibling z
+    _ -> pure $ z
+  _ -> pure $ z
 
 nextSibling :: Z.Zipper ValueF a -> Z.Zipper ValueF a
 nextSibling z = fromMaybe z $ do
@@ -135,22 +113,32 @@ prevSibling z = fromMaybe z $ do
     NullF -> Nothing
   Z.down newI parent
 
-into :: Z.Zipper ValueF a -> Z.Zipper ValueF a
-into z = case (Z.branches z) of
-  ObjectF hm -> fromMaybe z $ do
-    key <- ((HM.keys hm) ^? _head)
-    Z.down (Key key) z
-  ArrayF vec -> fromMaybe z $ do
-    Z.down (Index 0) z
-  StringF txt -> z
-  NumberF sci -> z
-  BoolF b -> z
-  NullF -> z
+into :: MonadState FocusState m => Z.Zipper ValueF a -> m (Z.Zipper ValueF a)
+into z = do
+  FS{..}<- get
+  case (Z.branches z) of
+    (ObjectF hm) -> do
+      let fstKey = (HM.keys hm) ^? _head
+      case keySelection of
+        Nothing -> do
+          case fstKey of
+            Nothing -> pure z
+            Just k -> do
+              modify (\fs -> fs{keySelection=Just k})
+              pure z
+        Just k -> pure $ Z.tug (Z.down (Key k)) z
+    ArrayF vec ->
+      pure . fromMaybe z $ do
+        Z.down (Index 0) z
+    StringF txt -> pure z
+    NumberF sci -> pure z
+    BoolF b -> pure z
+    NullF -> pure z
 
 renderValue' :: Z.Zipper ValueF Vty.Image -> Vty.Image
 renderValue' z =
-  let newA = toImage 0 (colorText (flip Vty.withStyle Vty.reverseVideo)) (z ^. Z.unwrapped . _unwrap . to (fmap Comonad.extract))
-   in Z.foldSpine (toImage 0 (colorText id)) $ (z & Z.focus_ .~ newA)
+  let newA = toImage 2 (colorText (flip Vty.withStyle Vty.reverseVideo)) (z ^. Z.unwrapped . _unwrap . to (fmap Comonad.extract))
+   in Z.foldSpine (toImage 2 (colorText id)) $ (z & Z.focus_ .~ newA)
 
 toImage :: Int -> (Maybe Vty.Color -> Text -> Vty.Image) -> ValueF Image -> Image
 toImage i img (StringF t) = indentLine i (img (Just green) ("\"" <> t <> "\""))
@@ -164,7 +152,7 @@ colorText :: (Attr -> Attr) -> (Maybe Vty.Color) -> Text -> Vty.Image
 colorText mod col txt = Vty.text' (mod $ maybe Vty.defAttr (Vty.withForeColor Vty.defAttr) col) txt <|> colorFix
 
 renderedZipper :: Cofree ValueF x -> Cofree ValueF Image
-renderedZipper = Z.retag (toImage 0 (colorText id))
+renderedZipper = Z.retag (toImage 2 (colorText id))
 
 atom :: Text -> Vty.Image
 atom t = Vty.text' defAttr t
@@ -176,7 +164,7 @@ prettyArray i img vs =
 
 prettyObj :: Int -> (Maybe Vty.Color -> Text -> Vty.Image) -> HashMap Text Vty.Image -> Vty.Image
 prettyObj i img vs =
-  let inner :: [Image] = indented 1 (HM.foldrWithKey' (\k v a -> [Vty.string (Vty.withForeColor defAttr Vty.cyan) (show k) <|> Vty.text' defAttr ": ", v] ++ a) [] vs)
+  let inner :: [Image] = indented 2 (HM.foldrWithKey' (\k v a -> [Vty.string (Vty.withForeColor defAttr Vty.cyan) (show k) <|> Vty.text' defAttr ": ", v] ++ a) [] vs)
    in Vty.vertCat . indented i $
         ([img Nothing "{"] ++ inner ++ [img Nothing "}"])
 
